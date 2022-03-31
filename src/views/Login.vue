741d8d1b93d6e82a6a57e92116c277ba89f1495e
YYYutang <1036503272@qq.com>
2022-04-01 01:53:45 +0800

    new file:   .gitignore         new file:   README.md         new file:   babel.config.js         new file:   package-lock.json         new file:   package.json         new file:   public/favicon.ico         new file:   public/index.html         new file:   src/App.vue         new file:   src/assets/logo.png         new file:   src/components/HelloWorld.vue         new file:   src/main.js         new file:   src/router/index.js         new file:   src/store/index.js         new file:   src/views/Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <el-form :model="loginForm" :rules="rules" ref="form" class="loginContainer">
+        <h3 class="loginTitle">系统登陆</h3>
+      <el-form-item prop="username">
+          <el-input type="text" auto-complete="false" v-model="loginForm.username" placeholder="请输入用户名" @input="update($event)"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+          <el-input type="password" auto-complete="false" v-model="loginForm.password" placeholder="请输入密码" ></el-input>
+      </el-form-item>
+      <el-form-item prop="code">
+          <el-input type="text" auto-complete="false" v-model="loginForm.code" placeholder="点击图片更换验证码" style="width:250px;margin-right:5px"></el-input>
+            <img :src="captchaUrl">
+      </el-form-item>
+          <el-form-item >
+          <el-input ></el-input>
+
+      </el-form-item>
+            <el-checkbox v-model="checked" class="loginRemember">记住我</el-checkbox>
+            <el-button type="primary" style="width:100%" @click="submitlogin">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Login",
+    data(){
+        return{
+            captchaUrl:'',
+            loginForm:{
+                username:'',
+                password:'',
+                code:''
+            },
+              checked:true,
+              rules:{
+                  username:[
+                      {required:true,message:'请输入用户名',trigger:'blur'}
+                      ],
+                  password:[
+                      {required:true,message:'请输入密码',trigger:'blur'}
+                      ],
+                  code:[
+                      {required:true,message:'请输入验证码',trigger:'blur'}
+                       ]
+              }
+        }
+    },
+    methods:{
+        submitlogin(){
+            alert(111)
+        },
+      update(e){
+          this.$forceUpdate();
+
+      }
+    }
+}
+</script>
+
+<style>
+.loginContainer{
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin:180px auto;
+    width:350px;
+    padding:15px 35px 15px 35px;
+    background: white;
+    border:1px solid #eaeaee;
+    box-shadow: 0 0 25px #cac6c6;
+}
+.loginTitle{
+    margin: 0px auto 40px auto;
+    text-align: center;
+}
+.loginRemember{
+    text-align: left;
+    margin: 0px 0px 15px 0px;
+}
+</style>  
