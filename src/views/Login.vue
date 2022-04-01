acf105734e28e16ab2f66fa797dc5a0c076d9c41
unknown <1036503272@qq.com>
2022-04-01 20:29:42 +0800

    first commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <el-form :model="loginForm" :rules="rules" ref="form" class="loginContainer">
+    <el-form :model="loginForm" :rules="rules" ref="loginForm" class="loginContainer">
         <h3 class="loginTitle">系统登陆</h3>
       <el-form-item prop="username">
-          <el-input type="text" auto-complete="false" v-model="loginForm.username" placeholder="请输入用户名" @input="update($event)"></el-input>
+          <el-input type="text" auto-complete="false" v-model="loginForm.username" placeholder="请输入用户名" ></el-input>
       </el-form-item>
       <el-form-item prop="password">
           <el-input type="password" auto-complete="false" v-model="loginForm.password" placeholder="请输入密码" ></el-input>
@@ -11,10 +11,6 @@
       <el-form-item prop="code">
           <el-input type="text" auto-complete="false" v-model="loginForm.code" placeholder="点击图片更换验证码" style="width:250px;margin-right:5px"></el-input>
             <img :src="captchaUrl">
-      </el-form-item>
-          <el-form-item >
-          <el-input ></el-input>
-
       </el-form-item>
             <el-checkbox v-model="checked" class="loginRemember">记住我</el-checkbox>
             <el-button type="primary" style="width:100%" @click="submitlogin">登录</el-button>
@@ -49,12 +45,16 @@ export default {
     },
     methods:{
         submitlogin(){
-            alert(111)
+            this.$refs.loginForm.validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+          this.$message.error('请输入所有字段');
+            return false;
+          }
+        });
         },
-      update(e){
-          this.$forceUpdate();
-
-      }
+    
     }
 }
 </script>
